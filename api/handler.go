3821a576d893546b705c099b6187c85bd64a2b31
Ashish Jaiswal <jaiswalashish025@gmail.com>
2023-12-10 10:42:44 +0530

    created hadnler func for handeling sendSMS and verifySMS

--- api/handler.go
+++ api/handler.go
@@ -1,1 +1,59 @@
 package api
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"sms-verify/data"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+const appTimeOut = time.Second * 10
+
+func (app *Config) sendSMS() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		_, cancel := context.WithTimeout(context.Background(), appTimeOut)
+		var payload data.OTPData
+		defer cancel()
+
+		app.validateBody(ctx, &payload)
+
+		newData := data.OTPData{
+			PhoneNumber: payload.PhoneNumber,
+		}
+
+		_, err := app.twilioSendOTP(newData.PhoneNumber)
+		if err != nil {
+			app.errorJSON(ctx, err)
+			return
+		}
+
+		app.writeJSON(ctx, http.StatusAccepted, "otp sent successfully!")
+	}
+}
+
+func (app *Config) verifySMS() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		_, cancel := context.WithTimeout(context.Background(), appTimeOut)
+		var payload data.VerifyData
+		defer cancel()
+
+		app.validateBody(ctx, &payload)
+
+		newData := data.VerifyData{
+			User: payload.User,
+			Code: payload.Code,
+		}
+
+		err := app.twilioVerifyOTP(newData.User.PhoneNumber, newData.Code)
+		fmt.Println("err: ", err)
+		if err != nil {
+			app.errorJSON(ctx, err)
+			return
+		}
+
+		app.writeJSON(ctx, http.StatusAccepted, "OTP verified successfully!")
+	}
+}
